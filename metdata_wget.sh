e7066503786ef2fe87ce2b493cb78a2f12fe7cfd
Terrin Warren <terrinwarren@gmail.com>
2026-04-17 15:45:01 -0700

    script for obtaining gridMET data

---
+++ metdata_wget.sh
@@ -0,0 +1,65 @@
+#!/bin/bash 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2016.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2017.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2018.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2019.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2020.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2021.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2022.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2023.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2024.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2025.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vpd_2026.nc 
+
+
+
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2016.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2017.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2018.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2019.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2020.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2021.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2022.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2023.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2024.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2025.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/pr_2026.nc 
+
+
+
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2016.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2017.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2018.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2019.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2020.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2021.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2022.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2023.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2024.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2025.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/vs_2026.nc 
+
+
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2016.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2017.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2018.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2019.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2020.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2021.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2022.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2023.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2024.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2025.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/bi_2026.nc 
+
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2016.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2017.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2018.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2019.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2020.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2021.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2022.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2023.nc 
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2024.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2025.nc
+wget -nc -c -nd http://www.northwestknowledge.net/metdata/data/fm100_2026.nc
